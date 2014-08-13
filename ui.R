221127349eafa8096f84195080617bc68a631dca
kjspring <kevinjspring@gmail.com>
2014-08-13 04:05:08 -0500

    made UI navigate

--- ui.R
+++ ui.R
@@ -1,38 +1,58 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
-library(shiny)
-shinyUI(
-  fluidPage(
-    titlePanel("Patent Practitioner Search"),
-    
-    sidebarLayout(
-      sidebarPanel("",
-                   textInput("zip", 
-                             label = h5("Zip Code Search"),
-                             value = "21287"),
-                   hr(),
-                   sliderInput("radius",
-                               label=h5("Search Area (mi)"),
-                               min=1, max=100, value= 5 ),
-                   hr(),
-                   checkboxGroupInput("check", label = h5("Practitioner Type"), 
-                                      choices = c("Attorney" = "ATTORNEY", "Agent" = "AGENT"),
-                                      selected = c("ATTORNEY", "AGENT")),
-                   hr(),
-                   verbatimTextOutput("resultsNum")
+shinyUI(navbarPage("Local Patent Practitioner Locator",
+                   tabPanel("Search",
+                            fluidRow(
+                              column(12,        
+                                     fluidRow(
+                                       column(4,
+                                              fluidRow(
+                                                column(12,
+                                                       textInput("zip", 
+                                                                 label = h3("Zip Code Search"),
+                                                                 value = "21287"),
+                                                       hr()
+                                                )
+                                              ),
+                                              fluidRow(
+                                                column(12,
+                                                       sliderInput("radius",
+                                                                   label=h3("Search Area (mi)"),
+                                                                   min=1, max=100, value= 5 ),
+                                                       hr()
+                                                )
+                                              ),
+                                              fluidRow(
+                                                column(12,
+                                                       checkboxGroupInput("check", label = h3("Practitioner Type"), 
+                                                                          choices = c("Attorney" = "ATTORNEY", "Agent" = "AGENT"),
+                                                                          selected = c("ATTORNEY", "AGENT")),
+                                                       hr()
+                                                )
+                                              )
+                                       ),
+                                       column(width = 8,
+                                              br(),
+                                              htmlOutput("map")
+                                       )
+                                     )
+                              )
+                            ),
+                            fluidRow(
+                              column(12,
+                                     p(" ")
+                              )
+                            ),
+                            fluidRow(
+                              column(12, 
+                                     verbatimTextOutput("resultsNum")
+                              )
+                            ),
+                            fluidRow(
+                              column(12,
+                                tableOutput("searchTable")
+                                )
+                              )
                    ),
-      mainPanel( htmlOutput("map")
-                  )
-                ) ,
-    fluidRow(
-      tableOutput("searchTable")
-            )
-    
-        )
-)
-
+                   navbarMenu("Docs",
+                              tabPanel("User Instructions"),
+                              tabPanel("Data Analysis Docs"))
+))
